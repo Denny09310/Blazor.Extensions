1b48c3ea6740afbc0a492b6225df8c287c3f3118
Denny09310 <k.denny2000@gmail.com>
2024-09-10 18:45:30 +0200

    feat: restored options functionality, add custom classes override

---
+++ Blazor.Extensions.BottomSheet/Options/BottomSheetClassesOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Blazor.Extensions.BottomSheet.Options;
+
+public class BottomSheetClassesOptions
+{
+    public string? Body { get; set; }
+    public string? Container { get; set; }
+    public string? Content { get; set; }
+    public string? DragIcon { get; set; }
+    public string? Header { get; set; }
+    public string? Overlay { get; set; }
+}
---
+++ Blazor.Extensions.BottomSheet/Options/BottomSheetEventsOptions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+using System.Text.Json.Serialization;
+
+namespace Blazor.Extensions.BottomSheet.Options;
+
+public class BottomSheetEventsOptions
+{
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public DotNetCallbackReference? OnDidDismiss { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public DotNetCallbackReference? OnDidShow { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public DotNetCallbackReference? OnWillDismiss { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public DotNetCallbackReference? OnWillShow { get; set; }
+}
